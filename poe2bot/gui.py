a59ed5da3c94f38d09730eb01e67300a846a7910
Chris Walker <chris.ak.walker@gmail.com>
2026-08-07 13:02:59 -0700

    Change new-step default Hold from 30ms to 20ms

--- poe2bot/gui.py
+++ poe2bot/gui.py
@@ -367,7 +367,7 @@ class App(tk.Tk):
         self.step_key_var = tk.StringVar()
         self.step_delay_var = tk.StringVar(value="10")
         self.step_jitter_var = tk.StringVar(value="5")
-        self.step_hold_var = tk.StringVar(value="30")
+        self.step_hold_var = tk.StringVar(value="20")
         self.step_hold_jitter_var = tk.StringVar(value="10")
         self.step_repeat_var = tk.StringVar(value="1")
         for label, var, width in (
@@ -920,7 +920,7 @@ class App(tk.Tk):
         self.step_key_var.set("")
         self.step_delay_var.set("10")
         self.step_jitter_var.set("5")
-        self.step_hold_var.set("30")
+        self.step_hold_var.set("20")
         self.step_hold_jitter_var.set("10")
         self.step_repeat_var.set("1")
         self.step_repeat_combine_hold_var.set(False)
